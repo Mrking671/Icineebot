5cdbd2c1793ce3e8f7ef4ec0e6b3cd68db5c294d
Mrblackgodd <136906179+Mrblackgodd@users.noreply.github.com>
2023-07-05 13:41:36 +0530

    Update info.py

--- info.py
+++ info.py
@@ -36,8 +36,8 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "Mrblackgod")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL','' ))
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', '')
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL','-1001818522365' ))
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', '-1001955515603')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
 IMDB = is_enabled((environ.get('IMDB', "False")), False)
 SINGLE_BUTTON = is_enabled((environ.get('SINGLE_BUTTON', "True")), True)
