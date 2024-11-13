2d47c90f24b37cdc9a3f5e950e35d35689baf055
Mrking671 <telegrambot969797@gmail.com>
2024-11-13 21:20:30 +0530

    Update info.py

--- info.py
+++ info.py
@@ -31,7 +31,7 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Forwardbot:nehalsingh969797@cluster0.znh0u0i.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://nehal969797:nehalsingh969797@cluster0.7ccmpy4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Mrblackgod")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
